b9e5a10b32b714dc189b2e531fed039c53e2a567
ganeshkishore <ganeshkishore.s@bigthinkcode.com>
2026-05-07 11:00:02 +0530

    react table tanstack

--- frontend/src/pages/PassengerDetail.jsx
+++ frontend/src/pages/PassengerDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import API from '../services/Api'
 import { useParams } from 'react-router-dom'
+import {useReactTable,getCoreRowModel,flexRender} from "@tanstack/react-table"
 
 const PassengerDetail = () => {
     const {id}=useParams()
@@ -19,6 +20,18 @@ const PassengerDetail = () => {
     }
    }
    if (!data) return <p>Loading...</p>;
+   const columns=[
+ {header:"Source",accessorKey:"source"},
+ {header:"Destination",accessorKey:"destination"},
+ {header:"Travel Date",accessorKey:"travel_date"},
+]
+const table=useReactTable({
+ data: data?.Travel || [],
+ columns,
+ getCoreRowModel:getCoreRowModel()
+})
+
+   
   return (
     <div><h1>PassengerDetail</h1>
     
@@ -26,18 +39,50 @@ const PassengerDetail = () => {
     <h4>Travel History</h4>
     <table  border={1}
     style={{ borderCollapse: "collapse", width: "100%" }}>
-        <thead>
+        {/* <thead>
             <tr>
                 <th>source</th>
                 <th>destination</th>
                 <th>travel_date</th>
         </tr>
+        </thead> */}
+        <thead>
+            {table.getHeaderGroups().map((headerGroup)=>(
+                <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header)=>(
+                        <th key={header.id}>
+                            {flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                            )}
+                        </th>
+                    ))}
+                </tr>
+            ))}
         </thead>
-        <tbody>
+
+        {/* <tbody>
     {data.Travel.map((t,index)=>(
        <tr key={index}> 
        <td>{t.source}</td>   <td>{t.destination}</td>   <td>({t.travel_date})</td></tr>
-    ))}</tbody></table>
+    ))}</tbody>*/}
+
+    <tbody>
+  {table.getRowModel().rows.map((row) => (
+    <tr key={row.id}>
+      {row.getVisibleCells().map((cell) => (
+        <td key={cell.id}>
+          {/* {cell.getValue()} */}
+           {flexRender(
+    cell.column.columnDef.cell,
+    cell.getContext()
+  )}
+        </td>
+      ))}
+    </tr>
+  ))}
+</tbody>
+    </table> 
     </div>
   )
 }
